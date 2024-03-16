4a3e3d4791a821875721b100acf295537884bb97
ShinFunk <shinya.yoshida420@gmail.com>
2024-03-17 06:51:51 +1100

    Update manifest and nitro configuration files, and add Github login button to Login.vue

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>Log in</div>
+  <button
+    class="bg-blue-500 text-black font-bold py-2 px-4 rounded"
+    @click="login"
+  >
+    Log in with Github
+  </button>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {};
-  },
+<script setup lang="ts">
+// const { title } = useCourse();
+const supabase = useSupabaseClient();
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+  });
+  if (error) {
+    console.error(error);
+  }
 };
 </script>
 
